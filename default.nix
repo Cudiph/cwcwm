ca3bc05319b452f202d303038236a9457c311dd0
Yappaholic <sav.boyar@gmail.com>
2025-09-29 03:53:44 +0300

    nix: fix LUA_PATH and make plugins available

--- default.nix
+++ default.nix
@@ -8,6 +8,7 @@
   libxkbcommon,
   libinput,
   xxHash,
+  lua,
   luajit,
   luajitPackages,
   xwayland,
@@ -23,52 +24,71 @@
   libdrm,
   python3Minimal,
   boost,
-}:
-stdenv.mkDerivation {
-  pname = "cwcwm";
-  version = "nightly";
+  makeWrapper,
+}: let
+  luaEnv = luajit.withPackages (ps: [
+    ps.lgi
+  ]);
+in
+  stdenv.mkDerivation {
+    pname = "cwcwm";
+    version = "nightly";
 
-  src = builtins.path {
-    path = ./.;
-    name = "source";
-  };
+    src = builtins.path {
+      path = ./.;
+      name = "source";
+    };
 
-  nativeBuildInputs = [
-    gnumake
-    meson
-    ninja
-    pkg-config
-    wayland-protocols
-    wayland-scanner
-    git
-    python3Minimal
-    boost
-  ];
+    nativeBuildInputs = [
+      gnumake
+      meson
+      ninja
+      pkg-config
+      wayland-protocols
+      wayland-scanner
+      git
+      python3Minimal
+      boost
+      makeWrapper
+      luaEnv
+    ];
 
-  buildInputs = [
-    wayland
-    wlroots_0_19
-    hyprcursor
-    cairo
-    libxkbcommon
-    libinput
-    xxHash
-    xwayland
-    libxcb
-    libxcb-wm
-    luajit
-    luajitPackages.lgi
-    libdrm
-  ];
+    buildInputs = [
+      wayland
+      wlroots_0_19
+      hyprcursor
+      cairo
+      libxkbcommon
+      libinput
+      xxHash
+      xwayland
+      libxcb
+      libxcb-wm
+      luaEnv
+      libdrm
+    ];
 
-  mesonFlags = ["-Dplugins=true" "-Dtests=true"];
+    mesonFlags = ["-Dplugins=true" "-Dtests=true"];
 
-  meta = {
-    mainProgram = "cwc";
-    description = "Hackable wayland compositor";
-    homepage = "https://github.com/Cudiph/cwcwm";
-    license = lib.licenses.gpl3Plus;
-    maintainers = [];
-    platforms = lib.platforms.linux;
-  };
-}
+    LUA_CPATH = "${luaEnv}/lib/lua/${luajit.luaversion}/?.so";
+    LUA_PATH = "${luaEnv}/share/lua/${luajit.luaversion}/?.lua;;";
+
+    postInstall = ''
+      # Very long LUA_PATH, maybe fixable
+
+      wrapProgram "$out/bin/cwc" \
+      --set LUA_PATH '${luaEnv}/share/lua/5.1/?.lua;${luaEnv}/share/lua/5.1/?/init.lua;${placeholder "out"}/share/cwc/lib/?.lua;${placeholder "out"}/share/cwc/lib/?/init.lua' \
+      --set LUA_CPATH '${luaEnv}/lib/lua/5.1/?.so' \
+      --set CWC_PLUGIN_PATH "$out/share/cwc/plugins"
+    '';
+
+    passthru = {inherit luajit;};
+    meta = {
+      mainProgram = "cwc";
+      description = "Hackable wayland compositor";
+      homepage = "https://github.com/Cudiph/cwcwm";
+      license = lib.licenses.gpl3Plus;
+      maintainers = [];
+      platforms = lib.platforms.linux;
+    };
+  }
