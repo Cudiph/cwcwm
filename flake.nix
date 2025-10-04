402310d54a6046aa6d4dc065600246b6946db262
Yappaholic <153394729+Yappaholic@users.noreply.github.com>
2025-10-04 12:35:09 +0300

    nix: add flake.nix and package derivation (#37)
    * nix: add flake and derivation
    
    * nix: fix flake.nix buildInput typo
    
    * nix: add meson flags for plugins and tests
    
    * nix: fix LUA_PATH and make plugins available
    
    * nix: add NixOS module and move stuff to nix/
    
    * nix: fix nixos-module typo
    
    * nix: add providedSessions
    
    * nix: attempt at fixing tty launch #1

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+
+      flake = {
+        nixosModules.cwc = import ./nix/nixos-module.nix self;
+      };
+
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: let
+        inherit
+          (pkgs)
+          callPackage
+          ;
+        cwc = callPackage ./nix/default.nix {};
+        shellOverride = old: {
+          nativeBuildInputs = old.nativeBuildInputs ++ [];
+          buildInputs = old.buildInputs ++ [];
+        };
+      in {
+        packages.default = cwc;
+        overlayAttrs = {
+          inherit (config.packages) cwc;
+        };
+        packages = {
+          inherit cwc;
+        };
+        devShells.default = cwc.overrideAttrs shellOverride;
+        formatter = pkgs.alejandra;
+      };
+      systems = ["x86_64-linux"];
+    };
+}
---
+++ nix/default.nix
@@ -0,0 +1,119 @@
+{
+  lib,
+  stdenv,
+  wayland,
+  wlroots_0_19,
+  hyprcursor,
+  pango,
+  cairo,
+  gdk-pixbuf,
+  glib,
+  libxkbcommon,
+  libinput,
+  xxHash,
+  luajit,
+  gobject-introspection,
+  xwayland,
+  libxcb,
+  libxcb-wm,
+  meson,
+  ninja,
+  pkg-config,
+  wayland-protocols,
+  wayland-scanner,
+  git,
+  libdrm,
+  python3Minimal,
+  boost,
+  makeWrapper,
+  wrapGAppsHook,
+  gtk3Support ? false,
+  gtk3 ? null,
+}:
+assert gtk3Support -> gtk3 != null; let
+  luaEnv = luajit.withPackages (ps: [
+    ps.lgi
+  ]);
+  commonDeps =
+    [
+      gdk-pixbuf
+      pango
+      glib
+    ]
+    ++ lib.optional gtk3Support gtk3;
+in
+  stdenv.mkDerivation {
+    pname = "cwc";
+    version = "nightly";
+
+    src = builtins.path {
+      path = ../.;
+      name = "source";
+    };
+
+    nativeBuildInputs = [
+      meson
+      ninja
+      pkg-config
+      wayland-protocols
+      wayland-scanner
+      git
+      python3Minimal
+      boost
+      makeWrapper
+      wrapGAppsHook
+      gobject-introspection
+    ];
+
+    buildInputs =
+      [
+        wayland
+        wlroots_0_19
+        hyprcursor
+        cairo
+        libxkbcommon
+        libinput
+        xxHash
+        xwayland
+        libxcb
+        libxcb-wm
+        luaEnv
+        libdrm
+      ]
+      ++ commonDeps;
+
+    propagatedBuildInputs = commonDeps;
+
+    doCheck = true;
+
+    GI_TYPELIB_PATH = "${pango.out}/lib/girepository-1.0";
+
+    mesonFlags = ["-Dplugins=true" "-Dtests=true"];
+
+    LUA_CPATH = "${luaEnv}/lib/lua/${luajit.luaversion}/?.so";
+    LUA_PATH = "${luaEnv}/share/lua/${luajit.luaversion}/?.lua;;";
+
+    postInstall = ''
+      # Copied from @SK4G from #31
+
+      wrapProgram "$out/bin/cwc" \
+      --prefix LUA_PATH : "${luaEnv}/share/lua/5.1/?.lua;${luaEnv}/share/lua/5.1/?/init.lua;${placeholder "out"}/share/cwc/?.lua" \
+      --prefix LUA_CPATH : "${luaEnv}/lib/lua/5.1/?.so;${placeholder "out"}/lib/cwc/plugins/?.so" \
+      --prefix GI_TYPELIB_PATH : "${lib.makeSearchPath "lib/girepository-1.0" [gobject-introspection gtk3 pango gdk-pixbuf]}" \
+      --prefix XDG_DATA_DIRS : "$out/share:${gtk3}/share:${gdk-pixbuf}/share:${pango}/share" \
+      --prefix GIO_MODULE_DIR : "${glib}/lib/gio/modules" \
+    '';
+
+    passthru = {
+      providedSessions = ["cwc"];
+      inherit luajit;
+    };
+    meta = {
+      mainProgram = "cwc";
+      description = "Hackable wayland compositor";
+      homepage = "https://github.com/Cudiph/cwcwm";
+      license = lib.licenses.gpl3Plus;
+      maintainers = [];
+      platforms = lib.platforms.linux;
+    };
+  }
